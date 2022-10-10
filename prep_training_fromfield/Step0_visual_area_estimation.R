1791cd5be618f4885a02cf8623c29c94edc80a81
Zsofia Koma <komazsofi@gmail.com>
2022-10-10 16:42:37 +0200

    small changes

--- prep_training_fromfield/Step0_visual_area_estimation.R
+++ prep_training_fromfield/Step0_visual_area_estimation.R
@@ -4,6 +4,8 @@ library(ggplot2)
 library(units)
 library(randomcoloR)
 
+## agri-ecology data
+
 setwd("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/exploration_analysis_2/")
 poly=st_read("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/ZsofiaData2011_LandsatTrainingData/IMK2011_XY_CropAggregationCarbon_Repair.shp")
 
@@ -46,5 +48,25 @@ ggplot(polyareas_finest_order[1:25,], aes(x=reorder(EnglishTxt,perc), y=perc, fi
   geom_bar(stat="identity",show.legend = FALSE) +
   coord_flip()+
   theme_bw(base_size = 25)+
-  ylab("Percentage of area to all")+xlab("Aggregated classes")+
+  ylab("Percentage of area to all")+xlab("Classes")+
   scale_fill_manual(values=distinctColorPalette(length(unique(polyareas_finest_order$EnglishTxt))))
+
+## §3 data
+
+poly_2=st_read("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Landsat_crop/BES_NATURTYPER_SHAPE/BES_NATURTYPER.shp")
+
+polyareas_ph3=poly_2 %>% 
+  st_drop_geometry() %>%
+  group_by(Natyp_navn) %>% 
+  summarise(area = sum(area_km2))
+
+polyareas_ph3$perc=(polyareas_ph3$area/sum(polyareas_ph3$area))*100
+polyareas_ph3$Natyp_navn[5]<-"So"
+
+ggplot(polyareas_ph3, aes(x=reorder(Natyp_navn,perc), y=perc, fill=Natyp_navn)) + 
+  geom_bar(stat="identity",show.legend = FALSE) +
+  coord_flip()+
+  theme_bw(base_size = 25)+
+  ylab("Percentage of area to all")+xlab("Classes")+
+  scale_fill_manual(values=distinctColorPalette(length(unique(reorder(polyareas_ph3$Natyp_navn,polyareas_ph3$perc)))))
+
